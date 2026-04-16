4e697508588fe16b2b6901ef2f6949cf312012ff
Hridoy Kumar Ghosh <hrighosh31@gmail.com>
2026-04-16 21:30:11 +0600

    Update Array_searching.cpp

--- Array_searching.cpp
+++ Array_searching.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main() {
-    // Your code here
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 3;
+    int index = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index != -1) {
+        cout << "Element found at index: " << index << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
     return 0;
 }
